5c438936a2b60bc5b0148ea26c950497d95f5da3
wangzhiwen <wangzhiwen@51tiangou.com>
2019-12-26 16:59:18 +0800

    修复积分排行页面，上拉加载失效的BUG

--- pages/scorelevel/scorelevel.js
+++ pages/scorelevel/scorelevel.js
@@ -9,7 +9,6 @@ Page({
      */
     data: {
         rankInfoList: [],
-        scrollHeight: 0,
         pageNum: 1,
         rank: 0,
         username: "",
@@ -18,11 +17,15 @@ Page({
     },
 
     getRankInfoList: function(pageNum) {
+        wx.showLoading({
+            title: '',
+        });
         let _this = this;
         wx.request({
             url: host.BASE_URL + 'coin/rank/' + pageNum + '/json',
             method: 'GET',
             success: function(res) {
+                wx.hideLoading();
                 if (res.data.errorCode != 0) {
                     wx.showToast({
                         title: res.data.errorMsg,
@@ -76,42 +79,45 @@ Page({
         }
     },
 
-    getWindowSize: function () {
-        let _this = this;
+    // getWindowSize: function () {
+    //     let _this = this;
 
-        wx.getSystemInfo({
-            success: function (res) {
-                let clientHeight = res.windowHeight;
-                let clientWidth = res.windowWidth;
-                let rpxR = 750 / clientWidth;
+    //     wx.getSystemInfo({
+    //         success: function (res) {
+    //             let clientHeight = res.windowHeight;
+    //             let clientWidth = res.windowWidth;
+    //             let rpxR = 750 / clientWidth;
 
-                console.log("设备高度: ", clientHeight);
-                console.log("设备宽度: ", clientWidth);
+    //             console.log("设备高度: ", clientHeight);
+    //             console.log("设备宽度: ", clientWidth);
 
-                let height = clientHeight * rpxR;
-                let width = clientWidth * rpxR;
+    //             let height = clientHeight * rpxR;
+    //             let width = clientWidth * rpxR;
 
-                console.log("高度: ", height);
-                console.log("宽度: ", width);
+    //             console.log("高度: ", height);
+    //             console.log("宽度: ", width);
 
-                _this.setData({
-                    scrollHeight: height - 150,  // 150为 scroll-view margin-top 高度
-                });
-            }
-        });
-    },
+    //             _this.setData({
+    //                 scrollHeight: height - 150,  // 150为 scroll-view margin-top 高度
+    //             });
+    //         }
+    //     });
+    // },
 
     /**
      * Lifecycle function--Called when page load
      */
     onLoad: function (options) {
+        //设置下拉刷新背景色
+        wx.setBackgroundColor({
+            backgroundColor: "#F7F7F7"
+        });
         this.setData({
             rank: options.rank,
             username: options.username,
             level: options.level,
             score: options.score
         });
-        this.getWindowSize();
     },
 
     /**
@@ -157,11 +163,18 @@ Page({
      */
     onReachBottom: function () {
         console.log('上拉加载触发');
-        let pageNum = this.data.pageNum + 1;
-        this.setData({
-            pageNum: pageNum
-        });
-        this.getRankInfoList(this.data.pageNum);
+        if (this.data.pageNum < 4) {
+            let pageNum = this.data.pageNum + 1;
+            this.setData({
+                pageNum: pageNum
+            });
+            this.getRankInfoList(this.data.pageNum);
+        } else {
+            wx.showToast({
+                title: '积分榜单仅展示前120名',
+                icon: 'none'
+            });
+        }
     },
 
     /**
