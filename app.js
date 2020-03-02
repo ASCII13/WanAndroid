f9a54c35d05ac40f1642ebc597ef198fc226721e
wangzhiwen <wangzhiwen@51tiangou.com>
2020-03-02 17:24:51 +0800

    封装网络请求

--- app.js
+++ app.js
@@ -1,39 +1,109 @@
 //app.js
+
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+    onLaunch: function() {
+        wx.getSystemInfo({
+            success: (res) => {
+                this.globalData.systemInfo = res;
+                if (res.model.search('iphone X') !== -1) {
+                    this.globalData.isIpohoneX = true;
+                }
             }
-          })
+        });
+    },
+
+    baseUrl: 'https://www.wanandroid.com',
+
+    /**
+	 * http请求封装
+	 * @param method 请求方法类型
+	 * @param url 请求路径
+	 * @param data 请求参数
+	 * @param loading 请求加载效果 {0: 正常加载, 1: 表单提交加载效果 }
+	 * @param loadingMsg 请求提示信息
+	 */
+    httpBase: function(method, url, data, loading = false, loadingMsg) {
+        let _this = this;
+        let requestUrl = this.baseUrl + url;
+
+        if (loading) {
+            wx.showLoading({
+                title: loadingMsg || '提交中...',
+                mask: true
+            });
+        } else {
+            wx.showNavigationBarLoading();
         }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+
+        function request(resolve, reject) {
+            wx.request({
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                method: method,
+                url: requestUrl,
+                data: data,
+                success: function(result) {
+                    if (loading) {
+                        wx.hideLoading({
+                          complete: (res) => {},
+                        });
+                    } else {
+                        wx.hideNavigationBarLoading({
+                          complete: (res) => {},
+                        });
+                    }
+
+                    let res = result.data || {};
+                    let code = res.errorCode;
+
+                    if (code !== 0) {
+                        reject(res);
+                        
+                        if (res.message) {
+                            wx.showToast({
+                              title: res.message,
+                              icon: 'none'
+                            });
+                        }
+                    } else {
+                        resolve(res);
+                    }
+                },
+                fail: function(res) {
+                    reject(res);
+
+                    if (loading) {
+                        wx.hideLoading({
+                          complete: (res) => {},
+                        });
+                    } else {
+                        wx.hideNavigationBarLoading({
+                          complete: (res) => {},
+                        });
+                    }
+
+                    wx.showToast({
+                      title: '网络出错',
+                      icon: 'none'
+                    });
+                }
+            });
+        }
+        return new Promise(request);
+    },
+
+    httpGet: function(url, data, loading, loadingMsg) {
+        return this.httpBase('GET', url, data, loading, loadingMsg);
+    },
+
+    httpPost: function() {
+        return this.httpBase('POST', url, data, loading, loadingMsg);
+    },
+
+    globalData: {
+        systemInfo: null,
+        userInfo: null,
+        isIpohoneX: false
+    }
 })
