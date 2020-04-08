a5f216bcf50ad2e7d3fc1884d42d44c09eb05bd0
wangzhiwen <wangzhiwen@51tiangou.com>
2020-04-08 14:58:24 +0800

    解决 收藏文章页面 上拉加载无数据时，UI展示异常

--- pages/collectarticles/collectarticles.js
+++ pages/collectarticles/collectarticles.js
@@ -1,6 +1,5 @@
 // pages/collectarticles/collectarticles.js
 
-let host = require('../../utils/host.js');
 let utils = require('../../utils/util.js');
 let app = getApp();
 
@@ -10,124 +9,92 @@ Page({
      * Page initial data
      */
     data: {
-        articles: [],
+        dataList: [],
         pageNum: 0,
+        requesting: false,
+        end: false
     },
 
-    getArticles: function(pageNum) {
-        let _this = this;
-        wx.request({
-            url: host.BASE_URL + 'lg/collect/list/'+ pageNum + '/json',
-            method: 'GET',
-            header: {
-                'Cookie': wx.getStorageSync('cookie') 
-            },
-            success: function(res) {
-                if (res.data.errorCode != 0) {
-                    wx.showToast({
-                        title: res.data.errorMsg,
-                        icon: 'none'
+    longpressItem(e) {
+        let index = e.currentTarget.dataset.index;
+        wx.showModal({
+            content: '确定取消收藏此文章？',
+            success: (res) => {
+                if (res.confirm) {
+                    this.uncollect(index);
+                }
+            }
+        }); 
+    },
+
+    clickItem(e) {
+        let index = e.currentTarget.dataset.index;
+        let currentData = this.getCurrentData(index);
+        let url = encodeURIComponent(currentData.link);
+
+        wx.navigateTo({
+          url: `../detail/detail?url=${url}`,
+        });
+    },
+
+    uncollect(index) {
+        let currentData = this.getCurrentData(index);
+        let id = currentData.id;
+        let originId = currentData.originId === undefined ? -1 : currentData.originId;
+
+        let data = {
+            originId: originId
+        }
+        app.httpPost(`/lg/uncollect/${id}/json`, data).then(() => {
+            let dataList = this.data.dataList;
+            dataList.splice(index, 1);
+
+            this.setData({dataList: dataList});
+            utils.showToastWithoutIcon('已取消收藏');
+        });
+    },
+
+    getCurrentData(index) {
+        return this.data.dataList[index];
+    },
+
+    getList(type, currentPage) {
+        this.setData({requesting: true});
+
+        app.httpGet(`/lg/collect/list/${currentPage}/json`).then((res) => {
+            this.setData({requesting: false});
+            
+            if (type === 'refresh') {
+                this.setData({
+                    dataList: res.data.datas,
+                    pageNum: currentPage + 1
+                });
+            } else {
+                if (res.data.total > this.data.dataList.length) {
+                    this.setData({
+                        dataList: this.data.dataList.concat(res.data.datas),
+                        pageNum: currentPage + 1
                     });
                 } else {
-                    let oldData = _this.data.articles;
-                    let newData = res.data.data.datas;
-                    if (res.data.data.total > oldData.length) {
-                        _this.setData({
-                            articles: oldData.concat(newData),
-                            pageNum: pageNum
-                        });
-                    } else if (oldData.length == 0 && newData.length == 0) {
-                        return;
-                    } else {
-                        wx.showToast({
-                            title: '无更多数据',
-                            icon: 'none'
-                          });
-                    }
+                    this.setData({end: true});
                 }
-            },
-            fail: function() {
-                wx.showToast({
-                    title: '网络异常，请稍后再试',
-                    icon: 'none'
-                })
             }
-        })
-        wx.stopPullDownRefresh();
-    },
-
-    clickArticleItem: function(e) {
-        let index = e.currentTarget.dataset.current;
-        let detailLink = encodeURIComponent(this.data.articles[index].link);
-        wx.navigateTo({
-          url: '../detail/detail' + '?url=' + detailLink
         });
     },
 
-    longPressArticleItem: function(e) {
-        let _this = this;
-        let index = e.currentTarget.dataset.current;
-
-        wx.showModal({
-          content: '确定取消收藏此文章？',
-          success: function(res) {
-            if (res.confirm) {
-                _this.unCollectArticle(index);
-            } else {
-                return;
-            }
-          }
-        });
+    refresh() {
+        this.getList('refresh', 0);
     },
 
-    unCollectArticle: function(index) {
-        let id = this.data.articles[index].id;
-        let originId = this.data.articles[index].originId.length == 0 ? -1 : this.data.articles[index].originId;
-        let _this = this;
-
-        wx.request({
-          url: host.BASE_URL + 'lg/uncollect/' + id + '/json',
-          method: 'POST',
-          header: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'Cookie': wx.getStorageSync('cookie')
-          },
-          data: {
-            originId: originId
-          },
-          success: function(res) {
-              if (res.data.errorCode != 0) {
-                  wx.showToast({
-                    title: res.data.errorMsg,
-                    icon: 'none'
-                  });
-              } else {
-                  let articles = _this.data.articles;
-                  articles.splice(index, 1);
-
-                  _this.setData({
-                      articles: articles
-                  });
-                  wx.showToast({
-                    title: '取消收藏成功',
-                    icon: 'none'
-                  });
-              }
-          },
-          fail: function() {
-              wx.showToast({
-                title: '网络异常，请稍后重试',
-                icon: 'none'
-              });
-          }
-        });
+    more() {
+        this.getList('more', this.data.pageNum);
     },
 
     /**
      * Lifecycle function--Called when page load
      */
     onLoad: function (options) {
-        this.getArticles(this.data.pageNum);
+        this.getList('refresh', this.data.pageNum);
     },
 
     /**
@@ -162,20 +129,14 @@ Page({
      * Page event handler function--Called when user drop down
      */
     onPullDownRefresh: function () {
-        this.setData({
-            pageNum: 0,
-            articles: []
-        });
-        this.getArticles(this.data.pageNum);
+
     },
 
     /**
      * Called when page reach bottom
      */
     onReachBottom: function () {
-        console.log("触发上拉加载");
-        let pageNum = this.data.pageNum + 1;
-        this.getArticles(pageNum);
+
     },
 
     /**
